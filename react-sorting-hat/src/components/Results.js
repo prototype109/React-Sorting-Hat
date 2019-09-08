4106edccad4c521aabb27ff778ae3e33cb064e35
Brandon Nichols <brandonaronnichols@gamil.com>
2019-09-07 21:59:08 -0500

    added tally results that give random house when tied

--- react-sorting-hat/src/components/Results.js
+++ react-sorting-hat/src/components/Results.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 const Results = props => {
-    let highestPoint = 0;
     let houseName = '';
 
     const tallyResutls = () => {
-        props.res.forEach(result => {
-            if(result.points > highestPoint){
-                highestPoint = result.points;
-                houseName = result.houseName;
-            }
-                
-        })
+        let topScoresArr = [];
+        let tempArr = [];
+        let maxScore = 0;
+        tempArr = props.res.map(result => result.points);
+        maxScore = Math.max(...tempArr);
+        topScoresArr = props.res.filter(result => maxScore === result.points);
+
+        if(topScoresArr.length > 1){
+            const randomHouse = Math.floor(Math.random() * topScoresArr.length)
+            return topScoresArr[randomHouse];
+        } else
+            return topScoresArr[0];
     }
+
+    houseName = tallyResutls().houseName;
 
     return(
         <div>
-            {tallyResutls()}
             <h1>You Belong In House {houseName}</h1>
-            {console.log('Result: ', houseName, highestPoint)}
         </div>
     )
 }
